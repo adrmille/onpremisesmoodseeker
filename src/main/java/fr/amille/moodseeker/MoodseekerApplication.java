47afa7f54e243ea7fdad01048177f6101771835f
Adrian Mille <adrmille@users.noreply.github.com>
2019-03-02 13:45:45 +0100

    Initial Springboot application commit

---
+++ src/main/java/fr/amille/moodseeker/MoodseekerApplication.java
@@ -0,0 +1,13 @@
+package fr.amille.moodseeker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoodseekerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoodseekerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/amille/moodseeker/MoodseekerApplicationTests.java
@@ -0,0 +1,16 @@
+package fr.amille.moodseeker;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MoodseekerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
